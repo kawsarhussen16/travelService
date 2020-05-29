e4fc69c383dec1b6b0ebe774c70bb004b6414731
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-29 01:49:23 -0400

    Layout and other component imported in app file

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./layout/Layout";
+import Tour from "./components/Tour";
+import Bookmark from "./components/Bookmark";
+import ReviewOverview from "./components/ReviewOverview";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Tour />
+      <Bookmark />
+      <ReviewOverview />
+    </Layout>
   );
 }
 

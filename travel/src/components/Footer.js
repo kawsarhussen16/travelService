7948be91f5766ebd0a8cdb0f0bf23a0aa0aa28cd
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-29 01:45:46 -0400

    footer and Booking button added

---
+++ travel/src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <Button>Book Now</Button>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+const FooterContainer = styled.div`
+  z-index: 999;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
+const Button = styled.button`
+  width: 100%;
+  background: red;
+  color: white;
+  height: 40px;
+  font-size: 20px;
+  border-radius: 15px;
+  border: none;
+`;

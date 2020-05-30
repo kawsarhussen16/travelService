877135cf6d015b4c152c3d57a974a0c42f30cce1
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 16:52:43 -0400

    Minor update for date pattern

--- travel/src/components/Reviews.js
+++ travel/src/components/Reviews.js
@@ -4,27 +4,32 @@ import RatingStar from "./utils/Star";
 const Reviews = ({ reviews }) => {
   return (
     <ReviewContainer>
-      {reviews.map((item) => {
-        const { headline, username, date, comment, rating } = item;
-        return (
-          <div key={username}>
-            <div className="review-detail">
-              <p>
-                {" "}
-                <strong>{headline} </strong>
-              </p>
-              <div className="rating-star">
-                {" "}
-                <RatingStar value={rating} />
+      {reviews
+        ? reviews.map((item) => {
+            const { headline, username, date, comment, rating } = item;
+            let convertedDate = new Date(
+              date["seconds"] * 1000
+            ).toLocaleDateString();
+            return (
+              <div key={username}>
+                <div className="review-detail">
+                  <p>
+                    {" "}
+                    <strong>{headline} </strong>
+                  </p>
+                  <div className="rating-star">
+                    {" "}
+                    <RatingStar value={rating} />
+                  </div>
+                  <p className="user-tag">
+                    by {username} on {convertedDate}
+                  </p>
+                  <p>{comment}</p>
+                </div>
               </div>
-              <p className="user-tag">
-                by {username} on {date.toLocaleDateString()}
-              </p>
-              <p>{comment}</p>
-            </div>
-          </div>
-        );
-      })}
+            );
+          })
+        : "Loading"}
     </ReviewContainer>
   );
 };

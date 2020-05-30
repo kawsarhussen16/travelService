fa3915510f2fa873538dfaffa719ce24a971f70c
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 16:51:27 -0400

    RatingReview Reducer implemented to update store according to async action

--- travel/src/redux/rating-reviews/rating_reviews_reducer.js
+++ travel/src/redux/rating-reviews/rating_reviews_reducer.js
@@ -1,21 +1,32 @@
 import ratingReviewsActionTypes from "./ratingReviews.action";
-import { getReviews, getAverageRating } from "./rating_reviews.utils";
+import { getAverageRating } from "./rating_reviews.utils";
 const initialState = {
   avgRating: 0,
   totalReviews: 0,
   reviews: [],
+  isFetching: false,
+  err: undefined,
 };
 
 const ratingReviewsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ratingReviewsActionTypes.GET_REVIEWS:
-      let reviews = getReviews();
-      let ratings = getAverageRating(reviews);
-      state = { ...state, ...ratings };
-      return {
-        ...state,
-        reviews: reviews,
-      };
+    case ratingReviewsActionTypes.FETCH_REVIEWS_START:
+      return Object.assign({}, state, { isFetching: true });
+
+    case ratingReviewsActionTypes.FETCH_REVIEWS_SUCCESS:
+      let avg = getAverageRating(action.payload);
+      return Object.assign({}, state, {
+        isFetching: false,
+        reviews: action.payload,
+        avgRating: avg.avgRating,
+        totalReviews: avg.totalReviews,
+      });
+
+    case ratingReviewsActionTypes.FETCH_REVIEWS_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        err: action.payload,
+      });
     default:
       return state;
   }

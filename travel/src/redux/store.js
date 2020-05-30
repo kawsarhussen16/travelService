c7f54e0d3409f3b0e9a2baddeb48abd6e9b79faa
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 15:32:41 -0400

    Redux thunk added for async data flow

--- travel/src/redux/store.js
+++ travel/src/redux/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
-const middlewares = [logger];
-
+import thunk from "redux-thunk";
+const middleware = applyMiddleware(thunk, logger);
 // if (process.env.NODE_ENV === "development") {
 //   middlewares.push(logger);
 // }
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(middleware));
 
 export default store;

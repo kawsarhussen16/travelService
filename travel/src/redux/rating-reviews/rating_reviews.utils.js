b07908b116d819cc03c2e676fb67cac702385a2d
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 16:51:50 -0400

    unnecessary datas deleted

--- travel/src/redux/rating-reviews/rating_reviews.utils.js
+++ travel/src/redux/rating-reviews/rating_reviews.utils.js
@@ -1,6 +1,3 @@
-export const getReviews = () => {
-  return data;
-};
 export const getAverageRating = (data) => {
   let total = 0;
   let totalReviews = 0;
@@ -11,53 +8,3 @@ export const getAverageRating = (data) => {
   let avgRating = (total / totalReviews).toFixed(1);
   return { avgRating, totalReviews };
 };
-const data = [
-  {
-    username: "John",
-    headline: "Perfect Tour",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 5,
-  },
-  {
-    username: "Melissa",
-    headline: "Great adventure",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 4,
-  },
-  {
-    username: "David",
-    headline: "Not Bad",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 4,
-  },
-  {
-    username: "Donald",
-    headline: "Perfect with Melania",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 5,
-  },
-  {
-    username: "Barack",
-    headline: "Awesome experience",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 5,
-  },
-  {
-    username: "Hilary",
-    headline: "Could be better",
-    date: new Date(),
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    rating: 4,
-  },
-];

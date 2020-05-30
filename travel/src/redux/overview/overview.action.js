681155cfc4a727e4952302db77bf4092a089b2ac
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 15:36:29 -0400

    Overview actions, and async function  implemented

--- travel/src/redux/overview/overview.action.js
+++ travel/src/redux/overview/overview.action.js
@@ -1,9 +1,35 @@
+import { firestore } from "../../firebase/firebase";
 const overviewActionTypes = {
-  GET_OVERVIEW: "GET_OVERVIEW",
+  FETCH_OVERVIEW_START: "FETCH_OVERVIEW_START",
+  FETCH_OVERVIEW_SUCCESS: "FETCH_OVERVIEW_SUCCESS",
+  FETCH_OVERVIEW_FAILURE: "FETCH_OVERVIEW_FAILURE",
 };
 
-export const getOverview = () => ({
-  type: overviewActionTypes.GET_OVERVIEW,
+export const fetchOverviewStart = () => ({
+  type: overviewActionTypes.FETCH_OVERVIEW_START,
 });
+export const fetchOverviewSuccess = (overview) => {
+  return {
+    type: overviewActionTypes.FETCH_OVERVIEW_SUCCESS,
+    payload: overview,
+  };
+};
+export const fetchOverviewFailure = (err) => ({
+  type: overviewActionTypes.FETCH_OVERVIEW_FAILURE,
+  payload: err,
+});
+
+export const fetchOverviewAsync = () => (dispatch) => {
+  dispatch(fetchOverviewStart());
+  const collectionRef = firestore.collection("canyone");
+  dispatch(fetchOverviewStart());
+  collectionRef
+    .get()
+    .then((data) => {
+      const overview = data.docs.map((d) => d.data());
+      dispatch(fetchOverviewSuccess(overview[0]));
+    })
+    .catch((err) => dispatch(fetchOverviewFailure(err)));
+};
 
 export default overviewActionTypes;

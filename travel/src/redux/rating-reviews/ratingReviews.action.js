4b545bf5cb09bca617b9657a7cfa242e4d32df12
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 16:52:26 -0400

    Action Types and async function implemented

--- travel/src/redux/rating-reviews/ratingReviews.action.js
+++ travel/src/redux/rating-reviews/ratingReviews.action.js
@@ -1,9 +1,35 @@
+import { firestore } from "../../firebase/firebase";
 const ratingReviewsActionTypes = {
-  GET_RATINGS: "GET_RATINGS",
-  GET_REVIEWS: "GET_REVIEWS",
+  FETCH_REVIEWS_START: "FETCH_REVIEWS_START",
+  FETCH_REVIEWS_SUCCESS: "FETCH_REVIEWS_SUCCESS",
+  FETCH_REVIEWS_FAILURE: "FETCH_REVIEWS_FAILURE",
 };
 
 export const getReviews = () => ({
   type: ratingReviewsActionTypes.GET_REVIEWS,
 });
+
+export const fetchReviewsStart = () => ({
+  type: ratingReviewsActionTypes.FETCH_REVIEWS_START,
+});
+export const fetchReviewsSuccess = (reviews) => ({
+  type: ratingReviewsActionTypes.FETCH_REVIEWS_SUCCESS,
+  payload: reviews,
+});
+export const fetchReviewsFailure = (err) => ({
+  type: ratingReviewsActionTypes.FETCH_REVIEWS_FAILURE,
+  payload: err,
+});
+
+export const fetchReviewsAsync = () => (dispatch) => {
+  dispatch(fetchReviewsStart());
+  const collectionRef = firestore.collection("ratingAndReviews");
+  collectionRef
+    .get()
+    .then((data) => {
+      const reviews = data.docs.map((d) => d.data());
+      dispatch(fetchReviewsSuccess(reviews));
+    })
+    .catch((err) => dispatch(fetchReviewsFailure(err)));
+};
 export default ratingReviewsActionTypes;

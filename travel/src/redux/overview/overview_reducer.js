749a07aad6824f02c646fb7a70a089094036344d
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-05-30 01:33:30 -0400

    OVERVIEW REDUCER IMPLEMENTED

--- travel/src/redux/overview/overview_reducer.js
+++ travel/src/redux/overview/overview_reducer.js
@@ -1,25 +1,15 @@
 import overviewActionTypes from "./overview.action";
-
+import { getOverview } from "./overview.utils";
 const initialState = {
   overview: null,
 };
 
 const overviewReducer = (state = initialState, action) => {
   switch (action.type) {
-    case overviewActionTypes.GET_PRICE:
+    case overviewActionTypes.GET_OVERVIEW:
       return {
         ...state,
-        price: action.payload,
-      };
-    case overviewActionTypes.GET_DURATION:
-      return {
-        ...state,
-        price: action.payload,
-      };
-    case overviewActionTypes.GET_DESCRIPTIONS:
-      return {
-        ...state,
-        price: action.payload,
+        overview: getOverview(),
       };
     default:
       return state;

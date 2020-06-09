659a595d7d78b68daea166891638b5c427041f37
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-06-09 14:31:54 -0400

    "Ionic packges + CSS core imported to react Project"

--- travel/src/index.js
+++ travel/src/index.js
@@ -6,6 +6,22 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./redux/store.js";
 
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
